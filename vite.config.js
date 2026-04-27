6f50cd5e9b3dd3b13c4dcd3c994183ef4dc6702e
MC-JUD <recouvrement@metzecare.com>
2026-04-27 16:41:57 +0200

    Fix base path + transactions JSON

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Remplace 'metze-finance-app' par le nom exact de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/metze-finance-app/',
+  base: '/Metze-finance-app/',
 })
